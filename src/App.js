91a8f0c49971958db4138e4323de513f572817ad
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-17 22:50:32 -0500

    links tested and fixed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import {Link} from 'react-router-dom'
 function App() {
   return (
     <div>
-      <p>Test</p>
-      <Link to="https://suspicious-shirley-aff7c1.netlify.app/">Evntz</Link>
-      <Link to="https://erik-eyler.github.io/popular-words/">Popular Words</Link>
-      <Link to ="https://yr-weeknd.netlify.app/">Yr Weeknd</Link>
+      <a href="https://suspicious-shirley-aff7c1.netlify.app/">Evntz</a>
+      <a href="https://erik-eyler.github.io/popular-words/">Popular Words</a>
+      <a href="https://yr-weeknd.netlify.app/">Yr Weeknd</a>
 
 
 
