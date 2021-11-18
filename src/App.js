1f451a56671ff434d6aa7bb2b21127beab80d7c6
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-17 23:03:42 -0500

    added basic styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <a href="https://suspicious-shirley-aff7c1.netlify.app/">Evntz</a>
-      <a href="https://erik-eyler.github.io/popular-words/">Popular Words</a>
-      <a href="https://yr-weeknd.netlify.app/">Yr Weeknd</a>
-
-
-
+    <div className="main-container">
+      <div className="headline">
+        <h1>Erik Eyler</h1>
+      </div>
+        <div className="links-container">
+          <a href="https://suspicious-shirley-aff7c1.netlify.app/" className="link">Evntz</a>
+          <a href="https://erik-eyler.github.io/popular-words/" className="link">Popular Words</a>
+          <a href="https://yr-weeknd.netlify.app/" className="link">Yr Weeknd</a>
+        </div>
+      
     </div>
     
   );
